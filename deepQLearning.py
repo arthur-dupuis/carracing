c9285749f830f12d6205193500315bf02abebbc6
arthur-dupuis <arthur.dupuis@polytechnique.edu>
2020-03-11 19:04:56 +0100

    step

---
+++ deepQLearning.py
@@ -0,0 +1,13 @@
+from history import History
+
+
+class DeepQLearning:
+    def __init__(self):
+        self.count = 0
+        self.history =
+
+    def run():
+        return
+
+    def build(self):
+        return
