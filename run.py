a65af2841ccffd29d4e21a878de654f0d9f5fabc
Arthur Dupuis <arthur.dupuis@polytechnique.edu>
2020-03-11 17:46:10 +0100

    first step

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 import gym
+from DeepQLearning import DeepQLearning as DQN
 
 env = gym.make("CarRacing-v0")
+
+trainings = 1000  # number of training episodes
+
+config = dict()
+
+agent = DQN(config)
+
+
+def run_once():
+
+
+def main():
+    while True:
+        if count > trainings:
+            break
+        run_once()
