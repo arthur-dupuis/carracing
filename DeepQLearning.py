b5e5c186cbf22316d037880dedd4f58bcb22e8fa
arthur-dupuis <arthur.dupuis@polytechnique.edu>
2020-03-11 19:03:39 +0100

    first steps

--- DeepQLearning.py
+++
@@ -1,8 +0,0 @@
-class config:
-    def __init__(self):
-        super().__init__()
-
-
-class DeepQLearning(config):
-    def __init__(self):
-        super().__init__()
--- run.py
+++ run.py
@@ -1,20 +1,28 @@
 import gym
-from DeepQLearning import DeepQLearning as DQN
+from DeepQLearning import DeepQLearning as DQL
 
 env = gym.make("CarRacing-v0")
 
 trainings = 1000  # number of training episodes
+saving_frequence = 400  # frequence of saving checkpoints
 
 config = dict()
 
-agent = DQN(config)
+agent = DQL(config)
+
+
+def save():
+    return
 
 
 def run_once():
+    reward, frames = agent.run()
+    if agent.count % saving_frequence == 0:
+        save()
 
 
 def main():
     while True:
-        if count > trainings:
+        if agent.count > trainings:
             break
         run_once()
